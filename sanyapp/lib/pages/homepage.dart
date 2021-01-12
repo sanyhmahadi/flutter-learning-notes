7df3135ba306112029a8b5e7e125a488756100e0
Your Name <you@example.com>
2021-01-13 00:59:42 +0600

    List View builder with jason data

--- sanyapp/lib/pages/homepage.dart
+++ sanyapp/lib/pages/homepage.dart
@@ -13,146 +13,43 @@ class _HomePageState extends State<HomePage> {
   final _long11 =
       "Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019";
   List people = [
-    {"name": "Sany", "email": "sanykhan227@gmail.com"},
+    {"name": "Sany", "email": "sanykhan21227@gmail.com"},
+    {"name": "Sany1", "email": "sanykhan2127@gmail.com"},
+    {"name": "Sanyfa", "email": "sanykhan4227@gmail.com"},
+    {"name": "Sany12", "email": "sanykhan25g27@gmail.com"},
+    {"name": "Sany13", "email": "sanykhan2r27@gmail.com"},
+    {"name": "Sany53", "email": "sanykhan2fe27@gmail.com"},
+    {"name": "Sanyfh", "email": "sanykhan2y27@gmail.com"},
+    {"name": "Sany7", "email": "sanykhan22er7@gmail.com"},
+    {"name": "Sany4", "email": "sanykhan22r7@gmail.com"},
+    {"name": "Sany753", "email": "sanykhan22ge7@gmail.com"},
+    {"name": "Sany737", "email": "sanykhan22cw7@gmail.com"},
+    {"name": "Sany36", "email": "sanykhan2tet27@gmail.com"},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-          title: Center(
-        child: Text('Sany'),
-      )),
-      body: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-          Divider(),
-          ListTile(
-            leading: CircleAvatar(
-              child: Text("PS"),
-            ),
-            title: Text("Sany H. Mahadi"),
-            subtitle: Text("Co-Owner"),
-            onTap: () {},
-          ),
-        ],
-      ),
-    );
+        appBar: new AppBar(
+            title: Center(
+          child: Text('Sany'),
+        )),
+        body: ListView.builder(
+          itemCount: people.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: [
+                ListTile(
+                  leading: CircleAvatar(
+                    child: Text(people[index]["email"][10]),
+                  ),
+                  title: Text(people[index]["name"]),
+                  subtitle: Text(people[index]["email"]),
+                )
+              ],
+            );
+          },
+        ));
   }
 
   Widget _rowcell() {
