9b7c7e63b0d03234f16d6c5cfa4f8b345b2f4adf
Your Name <you@example.com>
2021-01-13 21:27:17 +0600

    Curd view and metarial wizerd

--- sanyapp/lib/pages/homepage.dart
+++ sanyapp/lib/pages/homepage.dart
@@ -2,7 +2,7 @@
 //import 'package:sanyapp/pages/helper.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,115 +10,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _long11 =
-      "Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019 Premium BD Streaming is the best Streaming Platform in BD, Since 2019";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: Center(child: Text('Sany')),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            Stack(
-              children: [
-                Image.asset("asset/images/pm.png"),
-                Positioned(
-                    left: 20,
-                    bottom: 250,
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      child: Image.asset("asset/images/sanylogo.jpg"),
-                    )),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ListTile(
-              leading: Icon(Icons.details),
-              title: Text("About Us"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.subscriptions),
-              title: Text("Subscription Price"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.watch),
-              title: Text("Watch Now"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text("Edit Profile"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text("Edit Profile"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text("Edit Profile"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text("Edit Profile"),
-            ),
-            Divider(),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text("Logout Now"),
-            ),
-            Divider(),
-          ],
-        ),
+      drawer: _homeDrawer(),
+      body: ListView(
+        children: [
+          Card(child: Text("Choose Your Item")),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+          _beautyblushed(),
+        ],
       ),
     );
   }
 }
 
-/*
-List people = [
-    {"name": "Sany", "email": "sanykhan21227@gmail.com"},
-    {"name": "Sany1", "email": "sanykhan2127@gmail.com"},
-    {"name": "Sanyfa", "email": "sanykhan4227@gmail.com"},
-    {"name": "Sany12", "email": "sanykhan25g27@gmail.com"},
-    {"name": "Sany13", "email": "sanykhan2r27@gmail.com"},
-    {"name": "Sany53", "email": "sanykhan2fe27@gmail.com"},
-    {"name": "Sanyfh", "email": "sanykhan2y27@gmail.com"},
-    {"name": "Sany7", "email": "sanykhan22er7@gmail.com"},
-    {"name": "Sany4", "email": "sanykhan22r7@gmail.com"},
-    {"name": "Sany753", "email": "sanykhan22ge7@gmail.com"},
-    {"name": "Sany737", "email": "sanykhan22cw7@gmail.com"},
-    {"name": "Sany36", "email": "sanykhan2tet27@gmail.com"},
-  ];
+Widget _homeDrawer() {}
 
- Widget _rowcell() {
-    return Row(
-      children: [
-        Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            color: Colors.black,
-          ),
-          child: Icon(
-            Icons.access_alarm_outlined,
-            color: Colors.blue,
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        )
-      ],
-    );
-  }
-}*/
+Widget _beautyblushed() {
+  return Material(
+    elevation: 50,
+    child: Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset("asset/images/sanyl.png"),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "BeautyBlushed Paris",
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                Text(
+                  "Ladies Bag = 10",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                Text(
+                  "LeapStick = 15",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                Text(
+                  "Watch = 25",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _cell() {}
