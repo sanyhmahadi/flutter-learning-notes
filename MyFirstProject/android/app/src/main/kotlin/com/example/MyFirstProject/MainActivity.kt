06d73034995a4eda1c036e7287b3787fe4b76747
Your Name <you@example.com>
2021-01-19 23:34:46 +0600

    last update

--- MyFirstProject/android/app/src/main/kotlin/com/example/MyFirstProject/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.MyFirstProject
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- MyFirstProject/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'ui/home.dart';
-
-void main() => runApp(new MaterialApp(
-      home: Scafolder(),
-    ));
